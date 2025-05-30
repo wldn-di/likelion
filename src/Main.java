ce033bfdf24e19cfa4ab830c99e3df4fcf27564b
wldn-di <jiwoo@jiwooui-MacBookAir.local>
2025-05-30 14:36:28 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int age = 25;
+        System.out.println(age);
+        age = 26;
+        System.out.println(age);
+        double salary = 500.75;
+        System.out.println(salary);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String name = "정지우";
+        System.out.println("정지우"==name);
+        System.out.println("정지우".equals(name));
+
+        String name2 = new String("정지우");
+        System.out.println(name == name2);
     }
 }
