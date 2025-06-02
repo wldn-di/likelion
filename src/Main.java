f31d423d46be57166b6f971aad7cbea92a3be184
wldn-di <jiwoo@jiwooui-MacBookAir.local>
2025-06-02 09:21:21 +0900

    static final

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        int age = 25;
-        System.out.println(age);
-        age = 26;
-        System.out.println(age);
-        double salary = 500.75;
-        System.out.println(salary);
 
-        String name = "정지우";
-        System.out.println("정지우"==name);
-        System.out.println("정지우".equals(name));
-
-        String name2 = new String("정지우");
-        System.out.println(name == name2);
     }
 }
