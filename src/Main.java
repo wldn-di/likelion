f4a4b3b487b7d21f245669ae655318b368806fcb
wldn-di <springbom0214@gmail.com>
2025-06-02 10:35:05 +0900

    연산자

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 
 public class Main {
     public static void main(String[] args) {
-          final String SSN = "991117-2111111";
+        int a = 10;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
+
+        int difference = a - b;
+        System.out.println("difference = " + difference);
+
+        int product = a * b;
+        System.out.println("product = " + product);
+
+        int quotient = a / b;
+        System.out.println("quotient = " + quotient);
+
+        int remainder = a % b;
+        System.out.println("remainder = " + remainder);
     }
 }
